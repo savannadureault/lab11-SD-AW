e1868925f3e0675f1fa640b6d6d91c2dcac5f48a
Savanna Dureault <savannadureault@Savannas-MacBook-Air.local>
2025-11-12 12:45:52 -0500

    hello

--- calculator.py
+++ calculator.py
@@ -1,1 +1,27 @@
 # this is a test comment
+
+import math
+
+def add(a, b):
+    return (a + b)
+
+def sub(a, b):
+    return (a - b)
+
+def mul(a, b):
+    return (a * b)
+
+def div(a, b):
+    if a == 0:
+        raise ZeroDivisionError
+    return(b / a)
+    # raise ZeroDivisionError if a == 0
+
+def log(a, b):
+    if a <= 0 or a == 1 or b<= 0:
+        raise ValueError
+    return (math.log(b)/ math.log(a))
+    # use math library + raise ValueError
+
+def exp(a, b):
+    return(a**b)
